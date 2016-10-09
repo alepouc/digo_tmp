d70c8085ae8a0da65d72cb55dfab93cd30a42e1c
Alexis Coupe <cysecmate@gmail.com>
2016-10-09 21:56:04 +1100

    Add Ajax action call + response in modal

--- app/webapp/web.py
+++ app/webapp/web.py
@@ -6,75 +6,84 @@ from neo4jrestclient.client import GraphDatabase
 import time
 import json
 from pprint import pprint
+import random
 
 
-#time.sleep(7)
 app = Flask(__name__)
-
 gdb = GraphDatabase("http://digo-db:7474/db/data", username="neo4j", password="debug")
 
-'''
-@app.route('/')
-def index():
-    return render_template('graph.html')
-'''
 
 
+def convertNeo4jJsonToSigma(neo4jJson):
+    '''
+    Convert Neo4j JSON to Sigma JSON
+    '''
+
+    nodes_j = []
+    edges_j = []
+
+    for row in neo4jJson:
+        if str(row['relationships']) != "[]":
+            edges_j.extend(row['relationships'])
+        nodes_j.extend(row['nodes'])
+
+    nodes_j = {"nodes": nodes_j}
+    edges_j = {"edges": edges_j}
+
+    nodes = []
+    for item in nodes_j['nodes']:
+        if item not in nodes:
+            nodes.append(item)
+
+
+    edges = []
+    for item in edges_j['edges']:
+        if item not in edges:
+            edges.append(item)
+
+    SigmaJSON = {
+                "nodes": nodes,
+                "edges" : edges }
+
+
+    for item in SigmaJSON["nodes"]:
+        item['x'] = random.random()
+        item['y'] = random.random()
+
+
+    for item in SigmaJSON["edges"]:
+        item['source'] = item.pop("startNode")
+        item['target'] = item.pop("endNode")
+
+    return jsonify(SigmaJSON)
+
+
+
 @app.route('/')
-def homepage():
+def get_homepage():
+    '''
+    Default template
+    '''
     return render_template("index.html")
 
 
 @app.route('/graph.html')
-def graphpage():
+def get_graphpage():
     return render_template("graph.html")
 
 
-@app.route("/graph")
-def get_graph():
+@app.route("/neo4jJson")
+def get_neo4jJson():
+    '''
+    Get Neo4j JSON
+    '''
     #query = 'MATCH p=shortestPath( (bacon:Person {name:"Kevin Bacon"})-[*]-(meg:Person {name:"Meg Ryan"}) ) RETURN p'
     #query = 'MATCH (people:Person)-[relatedTo]-(:Movie {title: "Cloud Atlas"}) RETURN people.name, Type(relatedTo), relatedTo'
     #query = 'MATCH (n) RETURN n'
-    query = 'MATCH (n)-[r]-(m) RETURN n, r, m limit 10'
-
+    query = 'MATCH (n)-[r]-(m) RETURN n, r, m limit 5'
     results = gdb.query(query, data_contents=True)
-
-    nodes = ""
-    edges = ""
-
-    for row in results.graph:
-        #pprint(results.graph)
-        if str(row['relationships']) != "[]":
-            edges += str(row['relationships']).split("[", 1)[1].rsplit("]", 1)[0]+","
-        nodes += str(row['nodes']).split("[", 1)[1].rsplit("]", 1)[0]+","
-
-
-    nodes = '{"nodes":['+nodes.replace("{'",'{"').replace(" '",' "').replace("'}",'"}').replace("' ",'" ').replace("['",'["').replace("']",'"]').replace("',",'",').replace("':",'":').rsplit(",", 1)[0]+']}'
-    edges = '{"edges":['+edges.replace("{'",'{"').replace(" '",' "').replace("'}",'"}').replace("' ",'" ').replace("['",'["').replace("']",'"]').replace("',",'",').replace("':",'":').rsplit(",", 1)[0]+']}'
-
-
-    json_list_nodes = json.loads(nodes)
-    json_list_edges = json.loads(edges)
-
-
-    new_list_nodes = []
-    for item in json_list_nodes['nodes']:
-        if item not in new_list_nodes:
-            new_list_nodes.append(item)
-
-
-    new_list_edges = []
-    for item in json_list_edges['edges']:
-        if item not in new_list_edges:
-            new_list_edges.append(item)
-
-
-    new_result = '{"nodes":['+str(json.dumps(new_list_nodes))+'],"edges":['+str(json.dumps(new_list_edges))+']}'
-    new_result = new_result.replace('[[','[').replace(']]',']')
-    new_result = new_result.replace('},]','}]').replace('startNode', 'source').replace('endNode', 'target')
-
-
-    return new_result
+    SigmaJSON = convertNeo4jJsonToSigma(results.graph)
+    return SigmaJSON
 
 
 
@@ -93,8 +102,8 @@ def get_transform_table():
 
 @app.route('/action')
 def get_actions():
-    time.sleep(1)
-    return "plop"
+    time.sleep(2)
+    return '{"actions":["action1", "action2"]}'
 
 
 
